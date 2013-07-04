8950438a0d61b8c8a1ed146f6af8abd4f5063358
andy645jh <andy645jh@gmail.com>
2013-07-04 18:17:17 -0500

    acceso a los indicadores del click

--- js/Funciones.js
+++ js/Funciones.js
@@ -1,86 +1,87 @@
- var NUM_ENLACES_SEGMENTO = 3;
+var NUM_ENLACES_SEGMENTO = 3;
 
-      var numEnlace = 1;
-      var posX;
-      var posY;
+var seccionActual = null;
+var numEnlace = 1;
+var posX;
+var posY;
 
-      var numNavs = $('nav').children().length;
-      var segmento = Math.ceil(numNavs / NUM_ENLACES_SEGMENTO);
+var numNavs = $('nav').children().length;
+var segmento = Math.ceil(numNavs / NUM_ENLACES_SEGMENTO);
 
-      $(function () {
-          cbpFixedScrollLayout.init();
-      });
+$(function () {
+    cbpFixedScrollLayout.init();
+});
 
-      function mostrar() {
+function mostrar(tag) {
 
-          //alert($(this).attr("title"));
-          posX = window.pageXOffset;
-          posY = window.pageYOffset;
+    //alert($tag);
+    posX = window.pageXOffset;
+    posY = window.pageYOffset;
 
-          $("body").css("overflow", "hidden");
-          $("#dialogo").show();
-          scrollTo(posX, posY);
-          //$("#dialogo").dialog({ modal: true });
-      }
+    $("body").css("overflow", "hidden");
+    $("#dialogo").show();
+    scrollTo(posX, posY);
+    
+}
 
-      function ocultar() {//aun no funciona este codigo
+function ocultar() {//aun no funciona este codigo
 
-          scrollTo(posX, posY);
-          $("body").css("overflow", "visible");
-          $("#dialogo").css("display", "none");
-          //$("#dialogo").dialog({ modal: true });
-      }
+    scrollTo(posX, posY);
+    $("body").css("overflow", "visible");
+    $("#dialogo").css("display", "none");
+    //$("#dialogo").dialog({ modal: true });
+}
 
-      function bajar() {
+function bajar() {
                    
-          if (numEnlace <= segmento) {
+    if (numEnlace <= segmento) {
 
-              numEnlace++;
-              var index = (NUM_ENLACES_SEGMENTO * (numEnlace - 1)) - 1;
+        numEnlace++;
+        var index = (NUM_ENLACES_SEGMENTO * (numEnlace - 1)) - 1;
 
-              $enlace = $('nav > a:eq(' + index + ')');
-              var itemNext = $enlace.attr("id");
+        $enlace = $('nav > a:eq(' + index + ')');
+        var itemNext = $enlace.attr("id");
              
-              $('nav').animate({  scrollTop: 44 * index }, 1000);
+        $('nav').animate({  scrollTop: 44 * index }, 1000);
               
-          } else {
+    } else {
 
-          }
+    }
 
-      }
+}
 
-      function subir() {       
+function subir() {       
 
-          if (numEnlace >= 1) {
-              numEnlace--;
-              var index = (NUM_ENLACES_SEGMENTO * (numEnlace - 1)) -1;
+    if (numEnlace >= 1) {
+        numEnlace--;
+        var index = (NUM_ENLACES_SEGMENTO * (numEnlace - 1)) -1;
 
-              $enlace = $('nav > a:eq(' + index + ')');
-              var itemNext = $enlace.attr("id");
-              $('nav').animate({  scrollTop: 44 * index }, 1000);
+        $enlace = $('nav > a:eq(' + index + ')');
+        var itemNext = $enlace.attr("id");
+        $('nav').animate({  scrollTop: 44 * index }, 1000);
              
-          } else {
+    } else {
 
-          }
+    }
 
-      }
+}
 
-      $(document).ready(function () {
-          $("#content div").hide();
-          $("#tabs li:first").attr("id", "current");
-          $("#content div:first").fadeIn();
+$(document).ready(function () {
+    $("#content div").hide();
+    $("#tabs li:first").attr("id", "current");
+    $("#content div:first").fadeIn();
 
-          $('#tabs a').click(function (e) {
-              e.preventDefault();
-              $("#content div").hide();
-              $("#tabs li").attr("id", "");
-              $(this).parent().attr("id", "current");
-              $('#' + $(this).attr('title')).fadeIn();
+    $('#tabs a').click(function (e) {
+        e.preventDefault();
+        $("#content div").hide();
+        $("#tabs li").attr("id", "");
+        $(this).parent().attr("id", "current");
+        $('#' + $(this).attr('title')).fadeIn();
 
-          });
-      });
+    });
+});
 
-      function mostrarInfoDestino(){
+function mostrarInfoDestino(){
 
-          alert("mostrar info destino");
-      }
+    alert("mostrar info destino");
+}
