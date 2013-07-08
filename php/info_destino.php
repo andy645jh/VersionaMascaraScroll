9ce9a05a833622e4ba1fb784a7b38694f3e02ef1
andy645jh <andy645jh@gmail.com>
2013-07-08 12:59:04 -0500

    subida del lunes al medio dia

--- php/info_destino.php
+++ php/info_destino.php
@@ -1,13 +1,29 @@
 <?php
 
 require_once "conexion.php";
+$idDestino = $_REQUEST['destino'];
+//$idDestino = 1;
+$txt='';
 
-$sql = "select * from destino";
+$sql = "select url_datos from destino where id_destino =".$idDestino;
 $res = mysql_query($sql, conectar());
-$txt;
-while ($row = mysql_fetch_array($res)) {
-    $txt .= "<li>".$row["nombre"]."</li>";
+
+$url = mysql_fetch_array($res);
+
+
+leerTxt($url['url_datos']);
+
+function leerTxt ($urlDatos) {
+   
+    $file = fopen($urlDatos, "r") or exit("No se encontro archivo!");    
+
+    while(!feof($file)){
+        $txt.= fgets($file). "<br />";
+    }      
+
+    echo $txt;
+    fclose($file);
 }
 
-echo $txt;
+echo utf8_decode($txt);
 ?>
